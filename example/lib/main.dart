7c9de10fcbad4624b463ec4f3e5c4457d9a0568c
Husain Fadhilah <--get>
2022-06-13 11:12:00 +0700

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,34 +25,90 @@ class _SlidableButtonDemoState extends State<SlidableButtonDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Slide this button to left or right.'),
-            SizedBox(height: 16.0),
-            SlidableButton(
-              width: MediaQuery.of(context).size.width / 3,
-              buttonWidth: 60.0,
-              color: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
-              buttonColor: Theme.of(context).primaryColor,
-              dismissible: false,
-              label: Center(child: Text('Slide Me')),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('Left'),
-                    Text('Right'),
-                  ],
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Slide this button to left or right.'),
+                      SizedBox(height: 16.0),
+                      HorizontalSlidableButton(
+                        initialPosition: SlidableButtonPosition.start,
+                        width: MediaQuery.of(context).size.width / 3,
+                        buttonWidth: 60.0,
+                        color: Theme.of(context)
+                            .colorScheme
+                            .secondary
+                            .withOpacity(0.5),
+                        buttonColor: Theme.of(context).primaryColor,
+                        dismissible: false,
+                        label: Center(child: Text('Slide Me')),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Left'),
+                              Text('Right'),
+                            ],
+                          ),
+                        ),
+                        onChanged: (position) {
+                          setState(() {
+                            if (position == SlidableButtonPosition.end) {
+                              result = 'Button is at the right';
+                            } else {
+                              result = 'Button is on the left';
+                            }
+                          });
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              onChanged: (position) {
-                setState(() {
-                  if (position == SlidableButtonPosition.right) {
-                    result = 'Button is on the right';
-                  } else {
-                    result = 'Button is on the left';
-                  }
-                });
-              },
+                VerticalDivider(),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Slide this button to top or bottom.'),
+                      SizedBox(height: 16.0),
+                      VerticalSlidableButton(
+                        initialPosition: SlidableButtonPosition.start,
+                        height: MediaQuery.of(context).size.height / 3,
+                        buttonHeight: 60.0,
+                        color: Theme.of(context)
+                            .colorScheme
+                            .secondary
+                            .withOpacity(0.5),
+                        buttonColor: Theme.of(context).primaryColor,
+                        dismissible: false,
+                        label: Center(child: Text('Slide Me')),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Top'),
+                              Text('Bottom'),
+                            ],
+                          ),
+                        ),
+                        onChanged: (position) {
+                          setState(() {
+                            if (position == SlidableButtonPosition.end) {
+                              result = 'Button is at the bottom';
+                            } else {
+                              result = 'Button is on the top';
+                            }
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
             SizedBox(height: 16.0),
             Text('Result:\n$result', textAlign: TextAlign.center)

8e13c57c46b9f3dd34b7d1cd9dd2bc459dea9ddf
Husain Fadhilah <husainazkas@gmail.com>
2023-06-07 09:59:12 +0700

    remove deprecated members

--- lib/slidable_button.dart
+++ lib/slidable_button.dart
@@ -2,5 +2,4 @@ library slidable_button;
 
 export 'src/enum/slidable_button_position.dart';
 export 'src/horizontal_slidable_button.dart';
-export 'src/slidable_button.dart';
 export 'src/vertical_slidable_button.dart';
--- lib/src/enum/slidable_button_position.dart
+++ lib/src/enum/slidable_button_position.dart
@@ -1,10 +1,5 @@
 /// Position of button which on the left or right
 enum SlidableButtonPosition {
-  @Deprecated('This api is deprecated, please use [start] or [end].')
-  left,
-  @Deprecated('This api is deprecated, please use [start] or [end].')
-  right,
-
   start,
   end,
 
